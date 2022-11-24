5c8286ab39845bd80393cf49933ef27c753d28bb
Brad Miller <bradm@twitter.com>
2022-11-23 16:40:34 -0800

    merge constants.py changes

--- static/sourcecode/constants.py
+++ static/sourcecode/constants.py
@@ -6,6 +6,7 @@ minRatingsNeeded = 5
 maxHistoricalValidRatings = 5
 crhThreshold = 0.40
 crnhThresholdIntercept = -0.05
+crnhThresholdNMIntercept = -0.15
 crnhThresholdNoteFactorMultiplier = -0.8
 tagFilteringPercentile = 95
 minAdjustedTagWeight = 1.5
@@ -64,6 +65,7 @@ ratingCreatedBeforePublicTSVReleasedKey = "ratingCreatedBeforePublicTSVReleased"
 
 # Timestamps
 deletedNoteTombstonesLaunchTime = 1652918400000  # May 19, 2022 UTC
+notMisleadingUILaunchTime = 1664755200000  # October 3, 2022 UTC
 publicTSVTimeDelay = 172800000  # 48 hours
 
 # Explanation Tags
@@ -162,7 +164,7 @@ notHelpfulTagsAndTieBreakOrder = [
   (11, notHelpfulSpamHarassmentOrAbuseTagKey),
   (1, "notHelpfulIrrelevantSources"),
   (3, "notHelpfulOpinionSpeculation"),
-  (6, "notHelpfulNoteNotNeeded"),
+  (6, notHelpfulNoteNotNeededKey),
 ]
 notHelpfulTagsTSVOrder = [tag for (tiebreakOrder, tag) in notHelpfulTagsAndTieBreakOrder]
 notHelpfulTagsAndTypesTSVOrder = [(tag, np.int64) for tag in notHelpfulTagsTSVOrder]
@@ -229,7 +231,7 @@ noteTSVTypeMapping = {col: dtype for (col, dtype) in noteTSVColumnsAndTypes}
 ratingTSVColumnsAndTypes = (
   [
     (noteIdKey, np.int64),
-    (participantIdKey, np.object),
+    (raterParticipantIdKey, np.object),
     (createdAtMillisKey, np.int64),
     ("version", np.int64),
     ("agree", np.int64),
@@ -314,8 +316,23 @@ scoredNotesColumns = (
   [noteIdKey, helpfulNumKey]
   + helpfulTagsTSVOrder
   + notHelpfulTagsTSVOrder
-  + [numRatingsKey, noteInterceptKey, noteFactor1Key, ratingStatusKey, firstTagKey, secondTagKey]
-  + [createdAtMillisKey, noteAuthorParticipantIdKey, activeRulesKey, activeFilterTagsKey]
+  + [
+    numRatingsKey,
+    noteInterceptKey,
+    noteFactor1Key,
+    ratingStatusKey,
+    firstTagKey,
+    secondTagKey,
+    createdAtMillisKey,
+    noteAuthorParticipantIdKey,
+    activeRulesKey,
+    activeFilterTagsKey,
+    classificationKey,
+  ]
+)
+
+auxilaryScoredNotesColumns = (
+  [noteIdKey, ratingWeightKey] + notHelpfulTagsAdjustedColumns + notHelpfulTagsAdjustedRatioColumns
 )
 
 noteModelOutputTSVColumnsAndTypes = [
@@ -346,7 +363,14 @@ raterModelOutputTSVColumnsAndTypes = [
   (ratedAfterDecision, np.int64),
   (notesCurrentlyRatedHelpful, np.int64),
   (notesCurrentlyRatedNotHelpful, np.int64),
-  (notesAwaitingMoreRatings, np.int64)
+  (notesAwaitingMoreRatings, np.int64),
+  (enrollmentState, np.int32),
+  (successfulRatingNeededToEarnIn, np.int64),
+  (authorTopNotHelpfulTagValues, np.str),
+  (timestampOfLastStateChange, np.int64),
+  (aboveHelpfulnessThresholdKey, np.int32),
+  (isEmergingWriterKey, np.bool_),
+  (aggregateRatingReceivedTotal, np.int64),
 ]
 raterModelOutputTSVColumns = [col for (col, dtype) in raterModelOutputTSVColumnsAndTypes]
 raterModelOutputTSVTypeMapping = {col: dtype for (col, dtype) in raterModelOutputTSVColumnsAndTypes}
